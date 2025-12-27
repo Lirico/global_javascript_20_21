4b192a2dc690f4a6d4b8ac32da668971cd0d2fe6
lirico <dominguezmatiasadrian@gmail.com>
2025-12-27 12:47:49 -0300

    clase 1

---
+++ clase_01/index.js
@@ -0,0 +1,88 @@
+
+
+
+/********** JAVASCRIPT ********/
+/* 
+    Nace en 1995 -> Netscape
+    Fue creado para dar interactividad a los sitios webs.
+    Su creador fue Brendan Eich.
+
+    Cuando fabricamos una aplicación solemos requerir de al 
+    menos 3 lenguajes.
+    Uno para el frontend
+    Uno para el backend
+    Uno para las consultas a bases de datos.
+
+    Sorprendentemente, JavaScript es el unico lenguaje con el 
+    que se puede crear una app completa de punta a punta.
+    Eso se llama ISOMORFISMO.
+
+    ¿Que puedo programar con JS?
+    - Sitios webs -> React, Angular, VueJs, Svelte
+    - App web
+    - App mobile -> React Native
+    - App de escritorio -> Electron
+    - Videojuegos -> Phaser, Panda
+    - Programar hardware (incluso robotica) -> Jhonny Five
+    - Machine learning
+    - Chatbots
+    - Diseño 3D -> Three.js
+    - Dibujo 2D -> P5.js
+
+
+    JavaScript es un lenguaje de alto nivel.
+        -> Significa que su sintaxis es amigable con
+        la cognicion humana.
+*/
+
+
+/* TIPOS DE DATOS */
+/* 
+    TIPOS DE DATOS PRIMITIVOS
+        -> Aquel al cual se puede acceder a su valor de 
+        manera directa.
+            -> string (cadena de texto) -> "hola", 'hola'
+            -> number (numero) -> 32, -32, 3.2, -3.2
+            -> boolean (valores binarios) -> true/false
+            -> null -> Un valor vacio definido a drede
+            -> undefined -> Un valor vacio por defecto
+
+    TIPOS DE DATOS COMPUESTOS
+        -> Aquel al cual se accede a sus valores por referencia.
+            -> object (coleccion de variables privadas referenciadas por nombre de variable.)
+*/
+
+
+/* 
+    OPERADORES
+
+    UNARIOS
+        -> typeof -> Me permite conocer el tipo de un valor.
+
+    BINARIOS
+        -> Aritmeticos -> Devolver un number (unica excepcion -> +)
+            -> + suma
+            -> - resta
+            -> * multiplicacio
+            -> / divison
+            -> ** potencia
+            -> % modulo
+            -> ++ incrementadores
+            -> -- decrementadores
+        -> Comparativos -> Devuelven un booleano
+            -> >
+            -> <
+            -> >=
+            -> <=
+            -> == (iguladad abstracta)
+            -> === (igualdad estricta)
+            -> != (desigualdad abstracta)
+            -> !== (desigualdad estricta)
+        -> Logicos -> Devuelve un booleano
+            -> Y -> AND -> &&
+            -> O -> OR -> ||
+            -> Negacion -> NOT -> !
+
+    TERNARIOS (No los vamos a ver hoy.)
+*/
+ 
